b3a2b2e9e0c9322f3f99090c479f59bbf55d0123
John Osborn <bayreporta@gmail.com>
2016-05-17 19:43:32 -0700

    init commit

---
+++ Assets/OzTools/ProcessMath.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProcessMath : MonoBehaviour {
+
+    public static int Add_Nums(params int[] nums) {
+        int tot = 0;
+        foreach (int num in nums) {
+            tot += num;
+        }
+        return tot;
+    }
+}
---
+++ Assets/ParachuteKids.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using LitJson;
+
+public class ParachuteKids : MonoBehaviour {
+    private JsonData data;
+
+	// Use this for initialization
+	void Start () {
+        //data = ReadJson.Process_Data("/Resources/test.json");
+        data = ProcessMath.Add_Nums(34, 23, 235, 7756, 112, -2323, -44);
+        Debug.Log(data);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
